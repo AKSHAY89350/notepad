fe13381e4abae6eb782f1e340aaedd2adc7ebaab
AKSHAY89350 <57317439+AKSHAY89350@users.noreply.github.com>
2021-08-14 20:24:01 +0530

    Add files via upload

--- add.js
+++ add.js
@@ -3,16 +3,23 @@ let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click" , function(e){
 
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
+    
     if (notes == null){
         notesObj=[];
     }
     else{
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title:addTitle.value,
+        text:addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes",JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value ="";
     console.log(notesObj);
     showNotes();
 });
@@ -25,20 +32,20 @@ function showNotes(){
     else{
         notesObj = JSON.parse(notes);
     }
-    let html=""; 
-    notesObj.forEach( function(element,index) {
+    let html = ""; 
+    notesObj.forEach(function(element, index) {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Note ${index+1}</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">${element.text}</p>
         <button id=${index} onClick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
-      </div>`
+      </div>`;
     });
     let notesElm =document.getElementById("notes");
-    if(notesObj!=0){
-        notesElm.innerHTML=html;
+    if(notesObj.length != 0){
+        notesElm.innerHTML = html;
     }
     else{
         notesElm.innerHTML=`Nothing to show! use "Add a Note" section above to add notes.`;
